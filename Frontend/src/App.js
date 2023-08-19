d9dee83d060babe3e23224c478370ceaaba54247
Deepak Verma <deepakverma.knp2019@gmail.com>
2023-08-20 02:12:23 +0530

    components added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element = {<Home />} />
+        <Route path="/contact" element = {<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
