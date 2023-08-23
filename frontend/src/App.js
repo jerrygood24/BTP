d243a1b4f57988498eb4d59935b6bf5dbba99c63
Deepak Verma <deepakverma.knp2019@gmail.com>
2023-08-23 20:33:42 +0530

    login/signup created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import LoginPage from './pages/Login';
+import SignUp from './pages/Signup';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Route exact path="/" element = {<Home />} />
         <Route path="/about" element = {<About />} />
         <Route path="/contact" element = {<Contact />} />
+        <Route path="/login" element = {<LoginPage />} />
+        <Route path="/signup" element = {<SignUp />} />
       </Routes>
     </Router>
   );
