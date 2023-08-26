30786dd8a2404673db6c1bc9c30ab302ef9623cb
Deepak Verma <deepakverma.knp2019@gmail.com>
2023-08-26 23:12:08 +0530

    navbar reposnsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,17 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import LoginPage from './pages/Login';
 import SignUp from './pages/Signup';
+import Repository from './pages/Repository';
+import { CssBaseline, Box } from '@mui/material';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <Box sx={{ paddingTop: '64px' }}></Box>
       <Routes>
         <Route exact path="/" element = {<Home />} />
+        <Route path="/repository" element = {<Repository />} />
         <Route path="/about" element = {<About />} />
         <Route path="/contact" element = {<Contact />} />
         <Route path="/login" element = {<LoginPage />} />
