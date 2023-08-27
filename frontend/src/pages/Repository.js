14099dccde674356884e0b038dee58b257daadac
Deepak Verma <deepakverma.knp2019@gmail.com>
2023-08-28 01:38:51 +0530

    marzipano added

--- frontend/src/pages/Repository.js
+++ frontend/src/pages/Repository.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import Marzipano from 'marzipano';
+// import 'marzipano/dist/css/marzipano.css';
 
-const Repository = () => {
-    return <h2>Repository</h2>;
-    };
+const MarzipanoViewer = () => {
+  useEffect(() => {
+    console.log('Initializing Marzipano viewer...');
+    const viewerElement = document.getElementById('marzipano-viewer');
+    console.log('Viewer element:', viewerElement);
 
-export default Repository;
+    const viewer = new Marzipano.Viewer(viewerElement);
+
+    const source = Marzipano.ImageUrlSource.fromString('/b.jpeg');
+    const geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+    const limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
+    const view = new Marzipano.RectilinearView({}, limiter);
+
+    const scene = viewer.createScene({
+      source,
+      geometry,
+      view,
+      pinFirstLevel: true,
+    });
+
+    viewer.switchScene(scene);
+
+    console.log('Marzipano viewer initialized.');
+  }, []);
+
+  return <div id="marzipano-viewer" style={{ width: '100%', height: '500px' }}></div>;
+};
+
+export default MarzipanoViewer;
