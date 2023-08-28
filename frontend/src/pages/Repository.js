f60697ae99739067b4ea95a9d658443079d0a6c2
Deepak Verma <deepakverma.knp2019@gmail.com>
2023-08-28 12:24:29 +0530

    image rendered

--- frontend/src/pages/Repository.js
+++ frontend/src/pages/Repository.js
@@ -1,33 +1,57 @@
 import React, { useEffect } from 'react';
 import Marzipano from 'marzipano';
-// import 'marzipano/dist/css/marzipano.css';
 
 const MarzipanoViewer = () => {
   useEffect(() => {
     console.log('Initializing Marzipano viewer...');
-    const viewerElement = document.getElementById('marzipano-viewer');
-    console.log('Viewer element:', viewerElement);
+    // const viewerElement = document.getElementById('marzipano-viewer');
+    // console.log('Viewer element:', viewerElement);
 
-    const viewer = new Marzipano.Viewer(viewerElement);
+    const panoElement = document.getElementById('pano');
+    const viewerOpts = {
+      controls: {
+        mouseViewMode: 'drag'    // drag|qtvr
+      }
+    };
 
-    const source = Marzipano.ImageUrlSource.fromString('/b.jpeg');
-    const geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
-    const limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
+    const viewer = new Marzipano.Viewer(panoElement, viewerOpts);
+
+    const levels = [
+      { tileSize: 512, size: 512 },
+      { tileSize: 512, size: 1024 }
+    ];
+
+    const source = Marzipano.ImageUrlSource.fromString('http://localhost:3000/img/b3.jpeg');
+    // const geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+    const geometry = new Marzipano.EquirectGeometry(levels);
+    const limiter = Marzipano.RectilinearView.limit.traditional(1024, 100 * Math.PI / 180);
     const view = new Marzipano.RectilinearView({}, limiter);
 
     const scene = viewer.createScene({
-      source,
-      geometry,
-      view,
+      source: source,
+      geometry: geometry,
+      view: view,
       pinFirstLevel: true,
     });
 
-    viewer.switchScene(scene);
-
-    console.log('Marzipano viewer initialized.');
+    scene.switchTo({
+      transitionDuration: 1000
+    });
+    
+    var destinationViewParameters = {
+      yaw: 10 * Math.PI/180,
+      pitch: 15 * Math.PI/180,
+      fov: 60 * Math.PI/180
+    };
+    
+    var options = {
+      transitionDuration: 2000
+    }
+    
+    scene.lookTo(destinationViewParameters, options);
   }, []);
 
-  return <div id="marzipano-viewer" style={{ width: '100%', height: '500px' }}></div>;
+  return <div id="pano" style={{ width: '100%', height: '500px', background: '#ffcccc4d' }}></div>;
 };
 
 export default MarzipanoViewer;
