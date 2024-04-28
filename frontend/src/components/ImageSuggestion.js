c270f5a23708c16d60f5399bf6a6d00dd9e68174
Deepak Verma <deepakverma.knp2019@gmail.com>
2024-04-29 02:10:25 +0530

    image download

--- frontend/src/components/ImageSuggestion.js
+++ frontend/src/components/ImageSuggestion.js
@@ -12,7 +12,20 @@ const ImageSuggestion = ({ images }) => {
   const handleSearchChange = (event) => {
     setSearchKeyword(event.target.value);
   };
-
+  const handleImageClick = (imageUrl) => {
+    fetch(imageUrl, { headers: { "Content-Type": "image/jpeg" } })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "image.jpg");
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+      })
+      .catch((error) => console.error("Error downloading image:", error));
+  };
   return (
     <div className="image-suggestion-container">
       {/* Search input */}
@@ -31,7 +44,8 @@ const ImageSuggestion = ({ images }) => {
           <img
             src={image.imagePath}
             alt={`Suggested Image ${index}`}
-            style={{ width: "100%" }}
+            style={{ width: "100%", cursor: "pointer" }}
+            onClick={() => handleImageClick(image.imagePath)}
           />
           <CardContent>
             <Typography variant="body2" color="textSecondary">
